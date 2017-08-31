6c7890d557ae012eb57f06f3dd4aa48e3f90eaf3
Soeren Zorn <zsoerenm@hotmail.de>
2017-08-31 08:43:38 +0200

    Change .x back to []

--- src/ReactiveBasics.jl
+++ src/ReactiveBasics.jl
@@ -214,7 +214,7 @@ function flatten(input::Signal)
     subscribe!(updater, input.value)
     subscribe!(input) do u
         push!(signal, u.value)
-        unsubscribe!(updater, sigref.x)
+        unsubscribe!(updater, sigref[])
         subscribe!(updater, u)
         sigref[] = u
     end
